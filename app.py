73e872db8c4fc78d27470b4d495061d5f984b1d5
Said <s.lotus69@gmail.com>
2020-04-02 15:33:52 +0200

    feat:new class adding class show implementation one to many relation

--- app.py
+++ app.py
@@ -29,8 +29,6 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
-class 
-
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
@@ -42,7 +40,6 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
@@ -59,7 +56,12 @@ class Artist(db.Model):
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
-
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)   
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
